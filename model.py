aa18bc239a281dc987e96735eebe890afdc34976
celinemercierr <cmercier.ieu2023@student.ie.edu>
2026-03-26 08:42:16 +0100

    Update model.py

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class Down(nn.Module):
 class Up(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.up   = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.up   = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
         self.conv = ResDoubleConv(in_channels, out_channels)
 
     def forward(self, x, skip):
